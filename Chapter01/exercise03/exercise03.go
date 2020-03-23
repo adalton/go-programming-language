e43dedb7ac8865b71f36c9375e44f4354e5acadc
Andy Dalton <andy.dalton@gmail.com>
2020-03-23 00:27:46 -0400

    Move files around

---
+++ Chapter01/exercise03/exercise03.go
@@ -0,0 +1,25 @@
+// Copyright © 2016, 2019-2020 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// Modified by Andy Dalton to implement exercise solution.
+
+package exercise03
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func echo2() {
+	s, sep := "", ""
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+}
+
+func echo3() {
+	fmt.Println(strings.Join(os.Args[1:], " "))
+}
--- Chapter01/exercise03/exercise03_test.go
+++ Chapter01/exercise03/exercise03_test.go
@@ -1,38 +1,24 @@
-// Copyright © 2016, 2019 Alan A. A. Donovan & Brian W. Kernighan.
+// Copyright © 2016, 2019-2020 Alan A. A. Donovan & Brian W. Kernighan.
 // License: https://creativecommons.org/licenses/by-nc-sa/4.0/
 //
 // Modified by Andy Dalton to implement exercise solution.
 
-package main
+package exercise03_test
 
 import (
-	"fmt"
-	"os"
-	"strings"
 	"testing"
+
+	"exercise03"
 )
-
-func echo2() {
-	s, sep := "", ""
-	for _, arg := range os.Args[1:] {
-		s += sep + arg
-		sep = " "
-	}
-	fmt.Println(s)
-}
-
-func echo3() {
-	fmt.Println(strings.Join(os.Args[1:], " "))
-}
 
 func BenchmarkEcho2(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		echo2()
+		exercise03.echo2()
 	}
 }
 
 func BenchmarkEcho3(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		echo3()
+		exercise03.echo3()
 	}
 }
